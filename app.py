69c941e6720a2e1d3c85626877e465967519cd4d
KHARKHOUR Taoufik <tawfik.kharkhour@gmail.com>
2026-06-02 23:08:38 +0100

    fix: clean up app.py structure

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ def saluer(nom):
 def additionner(a, b):
     return a + b
 
-if __name__ == "__main__":
-    print(saluer("DevOps"))
-def diviser(a,b):
-if b == 0:
-raise ValueError("Division par zéro impossible")
-return a / b
-[200~import datetime
+def diviser(a, b):
+    if b == 0:
+        raise ValueError("Division par zéro impossible")
+    return a / b
+
+import datetime
 
 def log(message):
     ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"[{ts}] {message}")
+
+if __name__ == "__main__":
+    print(saluer("DevOps"))
